4b0e0cb50a9607525d130dd811980b816b56b80b
Certil Remy <certil@noukod.com>
2018-09-03 14:56:43 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,8 +2,7 @@ def using_while
 	levitation_force = 6
 	levitation_force = levitation_force +1
 
-	while levitation_force < 10
-
+	while levitation_force <= 10
 		puts "Wingardium Leviosa #{levitation_force}"
    break
 	end
